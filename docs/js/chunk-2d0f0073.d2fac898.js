d83514264613d8475be54a5320a5ae4cec373def
redgoose <scripter@me.com>
2021-05-30 00:19:34 +0900

    build

---
+++ docs/js/chunk-2d0f0073.d2fac898.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f0073"],{"9b2f":function(n,c,e){"use strict";e.r(c);var t=e("7a23");const a={d:"M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"};function o(n,c){return Object(t["openBlock"])(),Object(t["createBlock"])("path",a)}const r={};r.render=o;c["default"]=r}}]);
