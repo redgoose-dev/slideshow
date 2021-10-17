2846bb081b464bcb03ad4ddc859871cef7f03c68
redgoose <scripter@me.com>
2021-10-17 20:39:07 +0900

    props값 관련 수정

--- src/screen/Group/Item.vue
+++ src/screen/Group/Item.vue
@@ -1,25 +1,19 @@
 <template>
-<div :class="[
-  'group-item',
-  selected && 'group-item--selected'
-]">
+<div class="group-item">
   <a
-    :class="[
-      'group-item-wrap',
-      selected && 'group-item-wrap--selected',
-    ]"
+    :class="[ 'group-item-wrap', props.selected && 'group-item-wrap--selected' ]"
     @click="onSelectItem">
     <figure class="group-item__image">
-      <img v-if="src" :src="src" :alt="name">
+      <img v-if="props.src" :src="props.src" :alt="props.name">
       <Icon v-else icon-name="x"/>
     </figure>
     <div class="group-item__body">
-      <h3 :class="[ !name && 'none' ]">
-        {{ name ? name : 'None' }}
+      <h3 :class="[ !props.name && 'none' ]">
+        {{ props.name ? props.name : 'None' }}
       </h3>
-      <p>{{description}}</p>
-      <ul v-if="count !== undefined" class="group-item__meta">
-        <li><b>Count: {{count}}</b></li>
+      <p>{{props.description}}</p>
+      <ul v-if="props.count !== undefined" class="group-item__meta">
+        <li><b>Count: {{props.count}}</b></li>
       </ul>
     </div>
   </a>
